0b1826c3fec95fd52bd8f0dbefb0b5195223abae
Osama-alt-del <abdalghanyosama@gmail.com>
2026-03-31 00:59:35 +0900

    browser.tabs is undefined error
     - Looks like the script is being loaded in properly, but for some reason it doesn't know what browser.tab is?
     - Will search a little and try fix
     - The code only works when we search in the end, so the new tab page doesn't count as a URL anywhay. This means that I should probably find a different approach, but I need to get the API to work anyway, so I will try to do something with this "onCreated" function

--- newtabchange.js
+++ newtabchange.js
@@ -4,10 +4,15 @@ For now I will set the url manually and I can create popup later.
 */
 
 
+// Check to see if this file is even running:
+console.log("running");
+
+
 // Runs when new tab is created
 function handleCreated(tab) {
     console.log("New tab!!");
 }
 
 // We pass function in here, but what about parameter?
+// getting "browser.tabs is undefined" here:
 browser.tabs.onCreated.addListener(handleCreated);
