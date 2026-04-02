9e10e0590c5f9df38a6961f674af29e88552658d
Osama-alt-del <abdalghanyosama@gmail.com>
2026-04-03 04:49:35 +0900

    trying to complete the functionality but the toolbox debugger is not finding my popup.js file

---
+++ popup/popup.js
@@ -0,0 +1,9 @@
+function changeURL(){
+    element = document.getElementById("inputURL");
+    url = element.value;
+    // I wanna see what the value is
+    console.log(url);
+    // export the url so that it can be seen by the other file.
+    export url;
+}
+
