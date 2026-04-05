c7152bf4669b7abe8091622cb0d3a4ed5262eebb
Osama-alt-del <abdalghanyosama@gmail.com>
2026-04-06 01:28:17 +0900

    Onclick getting blocked by CSP
    - CSP doesn't allow inline JS and event handlers like "onclick" because they are very easy to exploit using XSS (cross-site scripting)
    - I should look how I can deal with all handlers in js files completely.

--- popup/popup.js
+++ popup/popup.js
@@ -1,9 +1,13 @@
+// At the start it's undefined, and we will check for undefined in other script.
+// Logging works, yay
+// console.log("test");
+var url = undefined;
 function changeURL(){
     element = document.getElementById("inputURL");
     url = element.value;
     // I wanna see what the value is
     console.log(url);
-    // export the url so that it can be seen by the other file.
-    export url;
 }
+
+export {url};
 
--- redirect.js
+++ redirect.js
@@ -1,1 +1,27 @@
-location="https://www.yahoo.co.jp/";
+// import the url variable from the popup
+import url from "./popup/popup.js";
+
+// I want to see what url is
+console.log(url);
+// If we have a url input, then just got there.
+if (url != undefined){
+    location = url;
+} else {
+    // If no url, we just get a blank screen...
+    console.log("no new tab");
+}
+
+
+
+```
+if (url != undefined){
+    // let gives us block scope, so we need to only ever define
+    let URL;
+    URL = url;
+}
+
+// If we have a url, then we define the location.
+if (url != undefined){
+    location="https://www.yahoo.co.jp/";
+}
+```
