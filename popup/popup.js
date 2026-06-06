9b91a2285d8a23edd25098d0ff95503bf7003d21
Osama-alt-del <abdalghanyosama@gmail.com>
2026-06-06 22:33:21 +0900

    using browser local storage to get the input to work

--- popup/popup.js
+++ popup/popup.js
@@ -1,13 +1,21 @@
-// At the start it's undefined, and we will check for undefined in other script.
-// Logging works, yay
-// console.log("test");
-var url = undefined;
-function changeURL(){
-    element = document.getElementById("inputURL");
-    url = element.value;
-    // I wanna see what the value is
-    console.log(url);
-}
 
-export {url};
+document.addEventListener("DOMContentLoaded", () => {
+    // So I think this function returns a promis, and when the promise is resolved, we can run the callback, which is the anonymous function that we have written here.
+    //just in case I want to search this later [??]
+    browser.storage.local.get("url").then((result) => {
+        if (result.url) {
+            document.querySelector(".currenURL").textContent = result.url;
+        }
+    });
 
+    // we only set using hte browser local storage if there is a click, and if there is an input
+    document.querySelector("button").addEventListener("click", () => {
+        const input = document.getElementById("inputURL").value;
+        if (input) {
+            // This part here I don't really understand [??]
+            browser.storage.local.set({ url: input }).then(() => {
+                document.querySelector(".currenURL").textContent = input;
+            });
+        }
+    });
+});
--- redirect.js
+++ redirect.js
@@ -1,27 +1,7 @@
-// import the url variable from the popup
-import url from "./popup/popup.js";
 
-// I want to see what url is
-console.log(url);
-// If we have a url input, then just got there.
-if (url != undefined){
-    location = url;
-} else {
-    // If no url, we just get a blank screen...
-    console.log("no new tab");
-}
-
-
-
-```
-if (url != undefined){
-    // let gives us block scope, so we need to only ever define
-    let URL;
-    URL = url;
-}
-
-// If we have a url, then we define the location.
-if (url != undefined){
-    location="https://www.yahoo.co.jp/";
-}
-```
+// Then we are going to get the url from the local storage and then change the url to the url that we set.
+browser.storage.local.get("url").then((result) => {
+    if (result.url) {
+        location = result.url;
+    }
+});
